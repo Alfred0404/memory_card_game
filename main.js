a048c5fb20177b7546c86cceb0d6428e4025db2b
Alfred de Vulpian <derfladv@gmail.com>
2023-08-11 18:06:22 +0200

    🏅Win condition added

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 console.log('main.js chargé');
 
 const cards = document.querySelectorAll('.card');
+const body = document.querySelector('body');
 console.log(cards);
 
 let cards_flipped = 0;
 
-const colors = {_red: '#ff0000', _green: '#00ff00', _black: '#000000'}
+const colors = {red: '#ff0000', green: '#00ff00', black: '#000000'}
 
 
 function randomise_cards() {
@@ -20,7 +21,6 @@ function randomise_cards() {
         p.textContent = num;
     });
 }
-
 
 function flip_2_cards() {
 
@@ -41,6 +41,7 @@ function flip_2_cards() {
                     console.log(cards_couple);
 
                     check_couple(cards_couple);
+                    check_win();
                     cards_couple.length = 0;
                     cards_flipped = 0;
                 }
@@ -50,56 +51,62 @@ function flip_2_cards() {
 }
 
 function check_couple(couple) {
-    console.log('check_couple', couple);
 
     const card_1 = couple[0];
     const card_2 = couple[1];
 
+    const card_list = [card_1, card_2]
+
     const card_1_text = card_1.querySelector('p').textContent;
     const card_2_text = card_2.querySelector('p').textContent;
-
-    console.log("carte 1", card_1_text);
-    console.log("carte 2", card_2_text);
 
     if (card_1_text === card_2_text) {
 
         console.log('les cartes sont identiques');
 
-        card_1.style.borderColor = colors._green;
-        card_2.style.borderColor = colors._green;
+        card_list.forEach(card => {
+            card.style.borderColor = colors.green;
+            card.style.boxShadow = '0 0 10px 0 rgba(0, 255, 0, 0.5)';
+        });
     }
     else {
         console.log('les cartes sont différentes');
 
-        card_1.style.borderColor = colors._red;
-        card_2.style.borderColor = colors._red;
+        card_list.forEach(card => {
+            card.style.borderColor = colors.red;
+            card.style.boxShadow = '0 0 10px 0 rgba(255, 0, 0, 0.5)';
+        });
 
         setTimeout(() => {
 
-            card_1.classList.remove('is-flipped');
-            card_2.classList.remove('is-flipped');
-
-            card_1.style.borderColor = colors._black;
-            card_2.style.borderColor = colors._black;
+            card_list.forEach(card => {
+                card.classList.remove('is-flipped');
+                card.style.borderColor = colors.black;
+                card.style.boxShadow = 'none';
+            });
         }, 1000);
     }
 }
 
 
 function check_win () {
+    let win = true;
+
     cards.forEach(card => {
-        if (card.classList.contains('is-flipped')) {
-            return false;
+        if (!card.classList.contains('is-flipped')) {
+            win = false;
         }
-        return true;
     });
+
+    if (win) {
+        body.style.backgroundColor = 'rgb(66, 255, 66)';
+    }
 }
 
 function main() {
     randomise_cards();
     flip_2_cards();
-    const win = check_win();
-    console.log(win);
+    console.log(check_win());
 }
 
 main();
